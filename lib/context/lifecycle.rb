0d28bc2d20828081cf861a5207c03f787460538c
rick <technoweenie@technoweenie.local>
2008-11-01 17:00:40 -0700

    implement before(:all) and after(:all)

--- lib/context/lifecycle.rb
+++ lib/context/lifecycle.rb
@@ -1,7 +1,6 @@
 class Test::Unit::TestCase
   class << self
-    attr_accessor :before_each_callbacks
-    attr_accessor :after_each_callbacks
+    attr_accessor :before_each_callbacks, :before_all_callbacks, :after_each_callbacks, :after_all_callbacks
 
     # Add logic to run before the tests (i.e., a +setup+ method)
     #
@@ -9,8 +8,8 @@ class Test::Unit::TestCase
     #       @user = User.first
     #     end
     # 
-    def before(&block)
-      before_each_callbacks << block
+    def before(period = :each, &block)
+      send("before_#{period}_callbacks") << block
     end
     
     # Add logic to run after the tests (i.e., a +teardown+ method)
@@ -19,18 +18,22 @@ class Test::Unit::TestCase
     #       User.delete_all
     #     end
     #
-    def after(&block)
-      after_each_callbacks << block
+    def after(period = :each, &block)
+      send("after_#{period}_callbacks") << block
     end
   end
 
+  self.before_all_callbacks  = []
   self.before_each_callbacks = []
   self.after_each_callbacks  = []
+  self.after_all_callbacks   = []
 
   def self.inherited(child)
     super
+    child.before_all_callbacks  = before_all_callbacks.dup
     child.before_each_callbacks = before_each_callbacks.dup
     child.after_each_callbacks  = after_each_callbacks.dup
+    child.after_all_callbacks   = after_all_callbacks.dup
 
     child.class_eval do
       def setup
@@ -41,5 +44,24 @@ class Test::Unit::TestCase
         self.class.after_each_callbacks.each { |c| instance_eval(&c) }
       end
     end
+  end
+
+  def run_all_callbacks(period = :before)
+    previous_ivars = instance_variables
+    self.class.send("#{period}_all_callbacks").each { |c| instance_eval(&c) }
+    (instance_variables - previous_ivars).inject({}) do |hash, ivar|
+      hash.update ivar => instance_variable_get(ivar)
+    end
+  end
+
+  def set_values_from_callbacks(values)
+    values.each do |name, value|
+      instance_variable_set name, value
+    end
+  end
+
+private
+  def instance_variable_hash
+    instance_variables.inject({}) { |hash, var| }
   end
 end
--- test/test_lifecycle.rb
+++ test/test_lifecycle.rb
@@ -14,8 +14,16 @@ class TestLifecycle < Test::Unit::TestCase
     @c = 1
   end
 
+  before :all do
+    @e = 1
+  end
+
+  after :all do
+    @f = 1
+  end
+
   sample_test = context "lifecycle" do
-    attr_reader :a, :b, :c, :d
+    attr_reader :a, :b, :c, :d, :e, :f, :g, :h
 
     before do
       @a = 3
@@ -25,11 +33,19 @@ class TestLifecycle < Test::Unit::TestCase
       @d = 1
     end
 
+    before :all do
+      @g = 1
+    end
+
+    after :all do
+      @h = 1
+    end
+
     test "foo" do
     end
   end
 
-  context "With before/after blocks" do
+  context "With before/after :each blocks" do
     before do
       @result = Test::Unit::TestResult.new
       @test = sample_test.new("test_lifecycle_foo")
@@ -50,6 +66,31 @@ class TestLifecycle < Test::Unit::TestCase
 
     it "it runs after callbacks" do
       assert_equal 1, @test.d
+    end
+  end
+
+  context "With before/after :all blocks" do
+    before do
+      @result = Test::Unit::TestResult.new
+      @suite  = sample_test.suite
+      @suite.run(@result) { |c, v| }
+      @test   = @suite.tests.first
+    end
+
+    it "it runs inherited before callbacks in order" do
+      assert_equal 1, @test.e
+    end
+
+    it "it runs before callbacks in order" do
+      assert_equal 1, @test.g
+    end
+
+    it "it runs inherited after callbacks" do
+      assert_equal 1, @test.f
+    end
+
+    it "it runs after callbacks" do
+      assert_equal 1, @test.h
     end
   end
 end
