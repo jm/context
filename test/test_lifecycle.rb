8de644ff449e22b9ccc9488179160084a7125f0d
rick <technoweenie@gmail.com>
2008-11-03 13:46:28 -0800

    use readable lifecycle test ivar names

--- test/test_lifecycle.rb
+++ test/test_lifecycle.rb
@@ -2,43 +2,45 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestLifecycle < Test::Unit::TestCase
   before do
-    @a = 1
+    @inherited_before_each_var = 1
   end
 
   before do
-    @a = 2
-    @b = 1
+    @inherited_before_each_var = 2
+    @inherited_before_each_var_2 = 1
   end
 
   after do
-    @c = 1
+    @inherited_after_each_var = 1
   end
 
   before :all do
-    @e = 1
+    @inherited_before_all_var = 1
   end
 
   after :all do
-    @f = 1
+    @inherited_after_all_var = 1
   end
 
   sample_test = context "lifecycle" do
-    attr_reader :a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l
+    attr_reader :inherited_before_each_var, :inherited_before_each_var_2, :inherited_after_each_var, 
+      :after_each_var, :inherited_before_all_var, :inherited_after_all_var, :before_all_var, :after_all_var, 
+      :superclass_before_each_var, :superclass_after_each_var, :superclass_before_all_var, :superclass_after_all_var
 
     before do
-      @a = 3
+      @inherited_before_each_var = 3
     end
 
     after do
-      @d = 1
+      @after_each_var = 1
     end
 
     before :all do
-      @g = 1
+      @before_all_var = 1
     end
 
     after :all do
-      @h = 1
+      @after_all_var = 1
     end
 
     test "foo" do
@@ -46,50 +48,50 @@ class TestLifecycle < Test::Unit::TestCase
   end
 
   before do
-    @i = 1
+    @superclass_before_each_var = 1
   end
 
   after do
-    @j = 1
+    @superclass_after_each_var = 1
   end
 
   before :all do
-    @k = 1
+    @superclass_before_all_var = 1
   end
 
   after :all do
-    @l = 1
+    @superclass_after_all_var = 1
   end
 
   context "With before/after :each blocks" do
     before do
       @result = Test::Unit::TestResult.new
       @test = sample_test.new("test_lifecycle_foo")
-      @test.run(@result) { |c, v| }
+      @test.run(@result) { |inherited_after_each_var, v| }
     end
 
     it "it runs superclass before callbacks in order" do
-      assert_equal 1, @test.i
+      assert_equal 1, @test.superclass_before_each_var
     end
 
     it "it runs inherited before callbacks in order" do
-      assert_equal 3, @test.a
+      assert_equal 3, @test.inherited_before_each_var
     end
 
     it "it runs before callbacks in order" do
-      assert_equal 1, @test.b
+      assert_equal 1, @test.inherited_before_each_var_2
     end
 
     it "it runs superclass after callbacks" do
-      assert_equal 1, @test.j
+      assert_equal 1, @test.superclass_after_each_var
     end
 
     it "it runs inherited after callbacks" do
-      assert_equal 1, @test.c
+      assert_equal 1, @test.inherited_after_each_var
     end
 
     it "it runs after callbacks" do
-      assert_equal 1, @test.d
+      assert_equal 1, @test.after_each_var
     end
   end
 
@@ -97,32 +99,32 @@ class TestLifecycle < Test::Unit::TestCase
     before do
       @result = Test::Unit::TestResult.new
       @suite  = sample_test.suite
-      @suite.run(@result) { |c, v| }
+      @suite.run(@result) { |inherited_after_each_var, v| }
       @test   = @suite.tests.first
     end
 
     it "it runs superclass before callbacks in order" do
-      assert_equal 1, @test.k
+      assert_equal 1, @test.superclass_before_all_var
     end
 
     it "it runs inherited before callbacks in order" do
-      assert_equal 1, @test.e
+      assert_equal 1, @test.inherited_before_all_var
     end
 
     it "it runs before callbacks in order" do
-      assert_equal 1, @test.g
+      assert_equal 1, @test.before_all_var
     end
 
     it "it runs superclass after callbacks" do
-      assert_equal 1, @test.l
+      assert_equal 1, @test.superclass_after_all_var
     end
 
     it "it runs inherited after callbacks" do
-      assert_equal 1, @test.f
+      assert_equal 1, @test.inherited_after_all_var
     end
 
     it "it runs after callbacks" do
-      assert_equal 1, @test.h
+      assert_equal 1, @test.after_all_var
     end
   end
 end
